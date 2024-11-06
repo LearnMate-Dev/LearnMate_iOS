17e3bbe403ac2fb6511e5efd7e6908a528cb282a
Bibi-urssu <bibi.urssu@gmail.com>
2024-11-06 13:44:28 +0900

    setting: Base 파일 생성
    Base View, ViewController, Button, Router

--- LearnMate/LearnMate/Presentation/Base/BaseRouter.swift
+++ LearnMate/LearnMate/Presentation/Base/BaseRouter.swift
@@ -5,4 +5,15 @@
 //  Created by 박지윤 on 11/6/24.
 //
 
-import Foundation
+import UIKit
+
+final class BaseRouter {
+    // MARK: Properties
+    weak var viewController: UIViewController?
+    
+    // MARK: Routing
+    func presentSampleViewController() {
+        let sampleViewController = UIViewController()
+        viewController?.navigationController?.pushViewController(sampleViewController, animated: true)
+    }
+}
--- LearnMate/LearnMate/Presentation/Base/BaseViewController.swift
+++ LearnMate/LearnMate/Presentation/Base/BaseViewController.swift
@@ -5,4 +5,34 @@
 //  Created by 박지윤 on 11/6/24.
 //
 
-import Foundation
+import UIKit
+
+class BaseViewController: UIViewController, UIGestureRecognizerDelegate {
+    
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureSubviews()
+        makeConstraints()
+        viewTransition()
+        setNavigationItem()
+        view.backgroundColor = .white
+    }
+    
+    // MARK: Configuration
+    func configureSubviews() {
+    }
+    
+    // MARK: Layout
+    func makeConstraints() {
+    }
+
+    // MARK: View Transition
+    func viewTransition() {
+    }
+
+    // MARK: Navigation Item
+    func setNavigationItem() {
+    }
+}
