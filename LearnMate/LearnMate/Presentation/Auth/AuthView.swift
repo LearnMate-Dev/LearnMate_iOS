d250a102353016245bf56fbfba3843cd779a8e54
Bibi-urssu <bibi.urssu@gmail.com>
2024-11-06 14:30:59 +0900

    feat: Auth UI 구현

--- LearnMate/LearnMate/Presentation/Auth/AuthView.swift
+++ LearnMate/LearnMate/Presentation/Auth/AuthView.swift
@@ -5,4 +5,48 @@
 //  Created by 박지윤 on 11/6/24.
 //
 
-import Foundation
+import UIKit
+
+final class AuthView: BaseView {
+
+    // MARK: UI Components
+    private let logoImageView = UIImageView().then {
+        $0.image = LMImage.splash
+        $0.contentMode = .scaleAspectFit
+    }
+
+    private let signInButton = BaseButton().then {
+        $0.setDefaultButton(title: "로그인", titleColor: .black, backgroundColor: .lmMain)
+    }
+
+    private let signUpButton = BaseButton().then {
+        $0.setDefaultButton(title: "회원가입", titleColor: .white, backgroundColor: .black)
+    }
+
+    // MARK: Configuration
+    override func configureSubviews() {
+        backgroundColor = .white
+        addSubview(logoImageView)
+        addSubview(signInButton)
+        addSubview(signUpButton)
+    }
+
+    // MARK: Layout
+    override func makeConstraints() {
+        logoImageView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(273)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(282)
+        }
+
+        signInButton.snp.makeConstraints {
+            $0.bottom.equalTo(signUpButton.snp.top).offset(-22)
+            $0.horizontalEdges.width.equalToSuperview()
+        }
+
+        signUpButton.snp.makeConstraints {
+            $0.bottom.equalTo(safeAreaLayoutGuide).inset(30)
+            $0.horizontalEdges.width.equalToSuperview()
+        }
+    }
+}
--- LearnMate/LearnMate/Presentation/Auth/AuthViewController.swift
+++ LearnMate/LearnMate/Presentation/Auth/AuthViewController.swift
@@ -5,4 +5,28 @@
 //  Created by 박지윤 on 11/6/24.
 //
 
-import Foundation
+import UIKit
+
+class AuthViewController: BaseViewController {
+
+    // MARK: UI Components
+    private let authView = AuthView()
+    
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: Configuration
+    override func configureSubviews() {
+        view.addSubview(authView)
+    }
+
+    // MARK: Layout
+    override func makeConstraints() {
+        authView.snp.makeConstraints {
+            $0.verticalEdges.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview().inset(20)
+        }
+    }
+}
