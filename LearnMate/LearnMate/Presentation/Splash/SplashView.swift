cfab13dde8e6efe87065f1160c1c8f09931b3323
Bibi-urssu <bibi.urssu@gmail.com>
2024-11-06 14:17:12 +0900

    feat: SplashView UI 구현

--- LearnMate/LearnMate/Presentation/Splash/SplashView.swift
+++ LearnMate/LearnMate/Presentation/Splash/SplashView.swift
@@ -10,12 +10,23 @@ import UIKit
 final class SplashView: BaseView {
 
     // MARK: UI Components
+    private let splashImageView = UIImageView().then {
+        $0.image = LMImage.splash
+        $0.contentMode = .scaleAspectFit
+    }
 
     // MARK: Configuration
     override func configureSubviews() {
+        backgroundColor = .white
+        addSubview(splashImageView)
     }
 
     // MARK: Layout
     override func makeConstraints() {
+        splashImageView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(273)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(282)
+        }
     }
 }
